31be7259a141d2adcc48bfee751db5e5c574d39f
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-21 23:40:31 -0500

    Revised code

--- scripts/ch2.js
+++ scripts/ch2.js
@@ -16,7 +16,7 @@ for(i = 1; i <= 100; i++) {
 
         console.log("Haw!")
 
-    } else if(i % 3 == 0 && i % 5 == 0) {
+    } else if(i % 3 === 0 && i % 5 === 0) {
 
         console.log("Hee Haw!");
 
